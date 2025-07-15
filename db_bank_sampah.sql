241bbf7f83217a4c51dd38847ab831536ffdd339
Aminuddin Shofi Ashari <55632248+aminuddinshofiashari@users.noreply.github.com>
2025-07-15 17:51:51 +0700

    add db bank sampah

---
+++ db_bank_sampah.sql
@@ -0,0 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Jul 2025 pada 12.51
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bank_sampah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis_sampah`
+--
+
+CREATE TABLE `jenis_sampah` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `jenis_sampah`
+--
+
+INSERT INTO `jenis_sampah` (`id`, `nama`, `harga`) VALUES
+(1, 'Organik ', 2000),
+(2, 'Plastik', 1000),
+(3, 'Kertas', 0),
+(4, 'Besi', 0),
+(5, 'Kaca', 0),
+(8, 'test', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nasaba`
+--
+
+CREATE TABLE `nasaba` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `no_hp` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `nasaba`
+--
+
+INSERT INTO `nasaba` (`id`, `nama`, `alamat`, `no_hp`) VALUES
+(1, 'Yogi', 'mbuh gajelas', '8798879978');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_jenis` int(11) NOT NULL,
+  `berat` float NOT NULL,
+  `harga_per_kg` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`id`, `tanggal`, `id_jenis`, `berat`, `harga_per_kg`, `total`) VALUES
+(1, '2002-02-01', 1, 20, 2000, 40000),
+(2, '2022-01-01', 4, 2000, 2000, 4000000),
+(3, '2025-07-15', 4, 3, 10000, 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `setoran`
+--
+
+CREATE TABLE `setoran` (
+  `id` int(11) NOT NULL,
+  `id_nasaba` int(11) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `setoran`
+--
+
+INSERT INTO `setoran` (`id`, `id_nasaba`, `tanggal`) VALUES
+(1, 1, '2002-01-01'),
+(2, 1, '2002-01-01'),
+(3, 1, '2002-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `setoran_detail`
+--
+
+CREATE TABLE `setoran_detail` (
+  `id` int(11) NOT NULL,
+  `id_setoran` int(11) DEFAULT NULL,
+  `id_jenis_sampah` int(11) DEFAULT NULL,
+  `berat` float DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `setoran_detail`
+--
+
+INSERT INTO `setoran_detail` (`id`, `id_setoran`, `id_jenis_sampah`, `berat`, `harga`, `total`) VALUES
+(1, 3, 4, 1, 0, 0),
+(2, 3, 5, 1, 0, 0),
+(3, 3, 3, 1, 0, 0),
+(4, 3, 1, 1, 2000, 2000),
+(5, 3, 2, 1, 1000, 1000),
+(6, 3, 8, 1, 1000, 1000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `jenis_sampah`
+--
+ALTER TABLE `jenis_sampah`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `nasaba`
+--
+ALTER TABLE `nasaba`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_jenis` (`id_jenis`);
+
+--
+-- Indeks untuk tabel `setoran`
+--
+ALTER TABLE `setoran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_nasaba` (`id_nasaba`);
+
+--
+-- Indeks untuk tabel `setoran_detail`
+--
+ALTER TABLE `setoran_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_setoran` (`id_setoran`),
+  ADD KEY `id_jenis_sampah` (`id_jenis_sampah`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `jenis_sampah`
+--
+ALTER TABLE `jenis_sampah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `nasaba`
+--
+ALTER TABLE `nasaba`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `setoran`
+--
+ALTER TABLE `setoran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `setoran_detail`
+--
+ALTER TABLE `setoran_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_sampah` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `setoran`
+--
+ALTER TABLE `setoran`
+  ADD CONSTRAINT `setoran_ibfk_1` FOREIGN KEY (`id_nasaba`) REFERENCES `nasaba` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `setoran_detail`
+--
+ALTER TABLE `setoran_detail`
+  ADD CONSTRAINT `setoran_detail_ibfk_1` FOREIGN KEY (`id_setoran`) REFERENCES `setoran` (`id`),
+  ADD CONSTRAINT `setoran_detail_ibfk_2` FOREIGN KEY (`id_jenis_sampah`) REFERENCES `jenis_sampah` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
