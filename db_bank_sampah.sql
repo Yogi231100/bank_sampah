094066ce190d69f1e7a196474f5a978777ee777b
aminuddinshofiashari <aminuddin170302@gmail.com>
2025-07-19 14:04:11 +0700

    add database

--- db_bank_sampah.sql
+++ db_bank_sampah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Jul 2025 pada 12.51
+-- Waktu pembuatan: 19 Jul 2025 pada 09.02
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -57,12 +57,12 @@ CREATE TABLE `jenis_sampah` (
 --
 
 INSERT INTO `jenis_sampah` (`id`, `nama`, `harga`) VALUES
-(1, 'Organik ', 2000),
+(1, 'Logam', 2000),
 (2, 'Plastik', 1000),
 (3, 'Kertas', 0),
 (4, 'Besi', 0),
 (5, 'Kaca', 0),
-(8, 'test', 1000);
+(8, 'seng', 1000);
 
 -- --------------------------------------------------------
 
@@ -92,21 +92,41 @@ INSERT INTO `nasaba` (`id`, `nama`, `alamat`, `no_hp`) VALUES
 
 CREATE TABLE `penjualan` (
   `id` int(11) NOT NULL,
-  `tanggal` date NOT NULL,
-  `id_jenis` int(11) NOT NULL,
-  `berat` float NOT NULL,
-  `harga_per_kg` int(11) NOT NULL,
-  `total` int(11) NOT NULL
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `penjualan`
 --
 
-INSERT INTO `penjualan` (`id`, `tanggal`, `id_jenis`, `berat`, `harga_per_kg`, `total`) VALUES
-(1, '2002-02-01', 1, 20, 2000, 40000),
-(2, '2022-01-01', 4, 2000, 2000, 4000000),
-(3, '2025-07-15', 4, 3, 10000, 30000);
+INSERT INTO `penjualan` (`id`, `tanggal`) VALUES
+(1, '0001-01-01'),
+(2, '2002-01-01'),
+(3, '2025-07-19');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_detail`
+--
+
+CREATE TABLE `penjualan_detail` (
+  `id` int(11) NOT NULL,
+  `id_penjualan` int(11) NOT NULL,
+  `id_jenis_sampah` int(11) NOT NULL,
+  `berat` float NOT NULL,
+  `harga` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penjualan_detail`
+--
+
+INSERT INTO `penjualan_detail` (`id`, `id_penjualan`, `id_jenis_sampah`, `berat`, `harga`, `total`) VALUES
+(1, 1, 3, 1, 20000, 20000),
+(2, 2, 2, 2, 20000, 40000),
+(3, 3, 2, 1, 2000, 2000);
 
 -- --------------------------------------------------------
 
@@ -127,7 +147,8 @@ CREATE TABLE `setoran` (
 INSERT INTO `setoran` (`id`, `id_nasaba`, `tanggal`) VALUES
 (1, 1, '2002-01-01'),
 (2, 1, '2002-01-01'),
-(3, 1, '2002-01-01');
+(3, 1, '2002-01-01'),
+(4, 1, '2025-07-18');
 
 -- --------------------------------------------------------
 
@@ -154,7 +175,13 @@ INSERT INTO `setoran_detail` (`id`, `id_setoran`, `id_jenis_sampah`, `berat`, `h
 (3, 3, 3, 1, 0, 0),
 (4, 3, 1, 1, 2000, 2000),
 (5, 3, 2, 1, 1000, 1000),
-(6, 3, 8, 1, 1000, 1000);
+(6, 3, 8, 1, 1000, 1000),
+(7, 4, 4, 1, 0, 0),
+(8, 4, 5, 1, 0, 0),
+(9, 4, 3, 1, 0, 0),
+(10, 4, 1, 1, 2000, 2000),
+(11, 4, 2, 1, 1000, 1000),
+(12, 4, 8, 1, 1000, 1000);
 
 --
 -- Indexes for dumped tables
@@ -182,8 +209,15 @@ ALTER TABLE `nasaba`
 -- Indeks untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `penjualan_detail`
+--
+ALTER TABLE `penjualan_detail`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_jenis` (`id_jenis`);
+  ADD KEY `id_penjualan` (`id_penjualan`),
+  ADD KEY `id_jenis_sampah` (`id_jenis_sampah`);
 
 --
 -- Indeks untuk tabel `setoran`
@@ -229,26 +263,33 @@ ALTER TABLE `penjualan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT untuk tabel `penjualan_detail`
+--
+ALTER TABLE `penjualan_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT untuk tabel `setoran`
 --
 ALTER TABLE `setoran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `setoran_detail`
 --
 ALTER TABLE `setoran_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `penjualan`
+-- Ketidakleluasaan untuk tabel `penjualan_detail`
 --
-ALTER TABLE `penjualan`
-  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_sampah` (`id`);
+ALTER TABLE `penjualan_detail`
+  ADD CONSTRAINT `penjualan_detail_ibfk_1` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id`),
+  ADD CONSTRAINT `penjualan_detail_ibfk_2` FOREIGN KEY (`id_jenis_sampah`) REFERENCES `jenis_sampah` (`id`);
 
 --
 -- Ketidakleluasaan untuk tabel `setoran`
